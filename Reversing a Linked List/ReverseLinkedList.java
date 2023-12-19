87c0a73207143fd2fecec7036b0608ddbec39952
Krish Chauhan <chauhankrish986@gmail.com>
2023-12-19 19:00:07 +0530

    Reversing A Linked List

---
+++ Reversing a Linked List/ReverseLinkedList.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList;
+
+public class ReverseLinkedList {
+
+    static class Node {
+        String data;
+        Node next;
+
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public ReverseLinkedList() {
+        this.head = null;
+    }
+
+    public void reverseList() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev; 
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.head = new Node("1");
+        list.head.next = new Node("2");
+        list.head.next.next = new Node("3");
+        list.head.next.next.next = new Node("4");
+
+        System.out.println("Original Linked List:");
+        list.printList();
+
+        list.reverseList();
+
+        System.out.println("Reversed Linked List:");
+        list.printList();
+    }
+}
